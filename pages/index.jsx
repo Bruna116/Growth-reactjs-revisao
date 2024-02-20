1c32a1ad790bf85e19fb35f968ea584d13a0aaf4
Bruna116 <bruninha.charmosinha@hotmail.com>
2024-02-20 14:00:57 -0300

    Criado exemplo com scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,13 @@
 import Title from '../src/components/title/title'
+import Button from '../src/components/button/button'
 
 export default function HomePage() {
-  return <Title>Titulo da pagina</Title>
+  return (
+    <>
+      <Title>Titulo da pagina</Title>
+      <Button>Botão</Button>
+      <label>LABEL TESTE</label>
+      <button>Button</button>
+    </>
+  )
 }
